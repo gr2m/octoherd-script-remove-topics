4696eb209ec7f441fe7df1af4dd1fef6b67b7e40
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-10-04 13:43:04 -0700

    feat: initial version

---
+++ script.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * remove topic(s) from repositories
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ * @param {object} options
+ * @param {string} options.topic
+ */
+export async function script(octokit, repository, { topic }) {}
